bc34fc9eb5131d121090892b9c76e9ebd96c898c
Kadama190 <91019774+Kadama190@users.noreply.github.com>
2021-09-27 19:29:58 +0300

    Add files via upload

---
+++ Лаба 2 перероб/4 задание.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("The current date and time is" + System.DateTime.Now);
+            Console.WriteLine("Input x + \r");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double I = 4 * Math.Sin(15d * Math.Exp(x1 / 2d) + 10.2) - 9d * Math.Exp(-x1) + Math.Abs(x1 + 5d / 3d);
+            Console.WriteLine("x1 = {0} \t I = {1}", x1, I);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лаба 2 перероб/Прак 2 приклад 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Прак_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // вивести версію о. с.
+            OperatingSystem os = System.Environment.OSVersion;
+            Console.WriteLine("Platform: {0}", os.Platform);
+            System.Console.WriteLine("The current date and time is" + System.DateTime.Now); // Дата і час
+            System.Console.ReadLine();
+        }
+    }
+}
