67b4f926f9b5f62ebbadde2b5dd768dd74380064
Kadama190 <91019774+Kadama190@users.noreply.github.com>
2021-10-01 16:46:20 +0300

    Add files via upload

---
+++ Лаба 3 перероб/Лаба 3 завд 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A; // = 993,285;
+            int I; // = 112000;
+            double C; // = 0,00023;
+            bool l; // = false;
+            string name; // "Aleksandrovich";
+            Console.WriteLine("Введіть A");
+            A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть I");
+            I = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть C");
+            C = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть l");
+            l = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Введіть ім'я");
+            name = Console.ReadLine();
+            Console.WriteLine("Результати форматування \nName = {0,1} \tl = {1,5}", name, l);
+            System.Console.WriteLine("\nA = {0,1:f3} \tI = {1,7} \tC = {2,9:f5}", A, I, C);
+            System.Console.WriteLine("\nЩоб вийти натисніть Enter");
+            Console.ReadLine();
+            System.Console.ReadLine();
+
+
+           
+
+        }
+    }
+}
---
+++ Лаба 3 перероб/Лаба 3 завдання 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace laba_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode; // Розпізнавання укр. літер
+            string s;
+            double x, y;
+            StreamWriter f = new StreamWriter("lab3.res"); // виведення 
+            StreamReader f1 = new StreamReader("lab3.txt"); // введення
+            f.WriteLine("Таблиця значень\nI------------------------------I\nI      X     I     Функція     I\nI------------------------------I");
+        metka: s = f1.ReadLine();
+            if (s == null) goto metka1;
+            x = Convert.ToDouble(s);
+            y = Math.Exp(-x) * Math.Sin(Math.PI * x / 2d);
+            f.WriteLine("I x = {0:f3} I y = {1:e3} I \n", x, y);
+            goto metka;
+        metka1: f.WriteLine("I-------------------------------I\nРозробив Вернигора Владислав Олександрович {0} \n", s);
+            f.Close();
+            f1. Close();
+        }
+    }
+}
