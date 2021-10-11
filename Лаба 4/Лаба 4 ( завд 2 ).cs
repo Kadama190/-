0c85f8a56e966fdf9f48018340d619c41742b41d
Kadama190 <91019774+Kadama190@users.noreply.github.com>
2021-10-11 12:59:06 +0300

    Add files via upload

---
+++ Лаба 4/Лаба 4 ( завд 2 ).cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace laba4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+        m1: Console.WriteLine("Точка M по " + "X=");
+            float x = float.Parse((Console.ReadLine()));
+            Console.WriteLine("Точка M по " + "Y=");
+            float y = float.Parse((Console.ReadLine()));
+            if (y < 0)
+            {
+                if (y-x<2)
+                {
+                    if (x >= 0) N = 2;
+                    else N = 1;
+                }
+                else
+                {
+                    if (x >= 0) N = 4;
+                    else N = 3;
+                }
+            }
+            else
+            {
+                if (x >= 0) N = 4;
+                else N = 2;
+            }
+            Console.WriteLine('\t' + "Виходить:");
+            Console.WriteLine("M(" + x + ";" + y + ")" + " В області N=" + N);
+            Console.WriteLine('\n' + "Повторний ввод" + "Нажати любу клавішу" + '\n' + "Для завершення Enter");
+            string p = Console.ReadLine();
+            if (p != "") goto m1;
+
+
+
+        }
+    }
+}
