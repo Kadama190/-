42184a29ca7216d5e867d2304445776a9e35a2f9
Kadama190 <91019774+Kadama190@users.noreply.github.com>
2021-10-19 11:43:28 +0300

    Add files via upload

---
+++ лаба5/Лаба 5 задание 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace laba5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, S, a, b, c, d, i;
+                int k;
+        m1: Console.WriteLine("Ввести значення x");
+            x = double.Parse((Console.ReadLine()));
+            Console.WriteLine('\t' + "Промежуточные результаты" + '\n');
+            k = 1;
+            S = 1; b = 1;
+            c = 1;
+            a = x;
+            while (Math.Abs(a) >= 0.000001)
+            {
+                S += a;
+                d = x * c;
+                i = b;
+                b += 3;
+                c = x * x;
+                a = 3 * (d / i);    
+                k++;
+                Console.WriteLine('\t' + "Інтерація №" + k + '\n' + "a=" + a + "S=" + S + "\n");
+                y = Math.Pow(x, (1 / 3));
+                Console.WriteLine('\t' + "   Результати:" + '\n' + "Заданий x=" + x + '\n' + "Сумма ряду S=" + S + '\n' + "Кількість членів ряду" + k + '\n' + "Функція x=" + y + '\n');
+                Console.WriteLine('\n' + "Для повторго ввода" + "Натиснути любу клавішу" + '\n' + "Для завершення дії натиснути Enter");
+                string rep = Console.ReadLine();
+                if (rep != "") goto m1;
+            }
+
+             
+        }
+    }
+}
---
+++ лаба5/лаба5 задание 1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace laba5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter p = new StreamWriter("laba5.txt");
+            float a, x, y;
+            p.WriteLine("Результат");
+            for (a = 1.25f; a <= 2f; a += 0.25f)
+            {
+                p.WriteLine("a=" + a);
+                for (x = -2; x <= 2; x = +0.1f) ;
+                {
+                    if (a < 0)
+                    {
+                        p.WriteLine("Негативний");
+                    }
+                    if (Math.Sqrt(a) == 0)
+                    {
+                        p.WriteLine("На нуль ділити не можна");
+                    }
+                    else
+                    {
+                        y = (float)(1 - Math.Exp(-(x / Math.Pow(x, 2)) / a));
+                        p.WriteLine("   x=   " + x + '\t' + "   y=   " + y);
+                    }
+                }
+            }
+            p.Close();
+
+
+
+
+
+
+
+        }
+    }
+}
