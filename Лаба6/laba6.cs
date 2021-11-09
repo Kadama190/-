4e3769845bb66555b029d48254ca74a19c155731
Kadama190 <91019774+Kadama190@users.noreply.github.com>
2021-11-09 18:01:52 +0300

    Add files via upload

---
+++ Лаба6/laba6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace laba_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int dodatnyi_chysla = 0;
+            string k;
+            string strValue;
+            int[] iArray1 = new int[20];
+
+            for (j = 0; j < 20; j++)
+            {
+                strValue = Console.ReadLine();
+                iArray1[j] = Convert.ToInt32(strValue);
+                if (iArray1[j] >= 0)
+                {
+                    dodatnyi_chysla += 1;
+
+                }
+            }
+
+            for (j = 0; j < 20; j++)
+            {
+                Console.WriteLine("\n" + "  |  " + j + "  |  " + iArray1[j] + "  |  ");
+
+            }
+            Console.WriteLine("\n Количество положительных элементов = " + dodatnyi_chysla);
+        }
+    }
+}
